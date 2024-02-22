a9041538e5bdf9512317dcf0e2a4bc3860b065bc
Povarnitsina Sonya <73982948+povar0305@users.noreply.github.com>
2024-02-22 11:59:40 +0300

    wrapper + titles

--- app.vue
+++ app.vue
@@ -1,5 +1,45 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="wrapper">
+    <div class="row-info">
+      <h1 class="title">Жилфонд</h1>
+      <p>Пользователь</p>
+    </div>
   </div>
 </template>
+
+
+<style lang="scss">
+.wrapper {
+  padding: 50px;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Montserrat", sans-serif;
+
+}
+
+.row-info {
+  margin-bottom: 26px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  p {
+    margin: 0;
+    font-weight: 400;
+    font-size: 16px;
+    color: #333333;
+  }
+
+  .title {
+    color: #E31F24;
+    margin: 0;
+    font-weight: 700;
+    font-size: 32px;
+  }
+}
+
+
+</style>
